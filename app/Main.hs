cdb0ea31519a1381b7d60bcf13041b64cf6563c5
Nobuo Yamashita <nobsun@sampou.org>
2022-07-15 16:45:54 +0900

    add interactWithPrompt' using Chan

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,19 @@
 module Main where
 
+import Data.Char
 import System.Environment ( getArgs )
 import Data.List.Extra
 import Transition ( responser )
-import Interact ( interactWithPrompt )
+import Interact ( interactWithPrompt
+                , interactWithPrompt'
+                )
+
+-- main :: IO ()
+-- main = interactWithPrompt "? " ":quit" . responser =<< list (return "") (const . readFile) =<< getArgs
 
 main :: IO ()
-main = interactWithPrompt "? " ":quit" . responser =<< list (return "") (const . readFile) =<< getArgs
+main = interactWithPrompt' "? " ":quit" sample
+
+sample :: [String] -> [String]
+sample = ((toUpper <$>) <$>)
+
--- src/Interact.hs
+++ src/Interact.hs
@@ -1,7 +1,10 @@
 module Interact
     ( interactWithPrompt
+    , interactWithPrompt'
     ) where
 
+import Control.Concurrent
+import Control.Concurrent.Chan
 import System.IO.Unsafe
 import System.Console.Haskeline
 
@@ -9,6 +12,25 @@ interactWithPrompt :: String -> String -> ([String] -> [String]) -> IO ()
 interactWithPrompt prompt quit f 
     = putStr . unlines . f =<< loop
     where
+        loop :: IO [String]
+        loop = unsafeInterleaveIO $ do
+            { minput <- runInputT defaultSettings (getInputLine prompt)
+            ; case minput of
+                Nothing   -> return []
+                Just line
+                    | line == quit -> return []
+                    | otherwise    -> (line :) <$> loop
+            }
+
+interactWithPrompt' :: String -> String -> ([String] -> [String]) -> IO ()
+interactWithPrompt' prompt quit f = do
+    { o   <- newChan
+    ; tid <- forkIO (g o)
+    ; putStr . unlines =<< getChanContents o
+    }
+    where
+        g    :: Chan String -> IO ()
+        g o  = writeList2Chan o . f =<< loop
         loop :: IO [String]
         loop = unsafeInterleaveIO $ do
             { minput <- runInputT defaultSettings (getInputLine prompt)
