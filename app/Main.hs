5967d2c4447c09911b245e26c7ef7d1f88d159ff
Nobuo Yamashita <nobsun@sampou.org>
2022-07-27 23:53:06 +0900

    change api

--- app/Main.hs
+++ app/Main.hs
@@ -3,16 +3,74 @@ module Main where
 import Control.Concurrent.Chan
 import Control.Exception
 import Data.Char
-import Interact ( InputConfig (..), interact' ) 
-import Transition ( responder )
+import Data.List.Extra
+import Interact
+import System.Environment
 
 main :: IO ()
-main = do 
-    { req <- newChan
-    ; res <- newChan
-    ; catch (interact' (InputConfig "? " ":quit" Nothing) req res sample ((putStr . unlines =<<) . getChanContents))
-            (\ BlockedIndefinitelyOnMVar -> return ())
+main = (interactions !!) . (`mod` 8) 
+   =<< list (return 0) (((return . read) .) . const)
+   =<< getArgs 
+
+interactions :: [IO ()]
+interactions = [foo, foo', bar, bar', baz, baz', qux, qux']
+
+toUpperCase :: [String] -> [String]
+toUpperCase = map upcase
+
+upcase :: String -> String
+upcase = show . map (ord . toUpper)
+
+toUpperCaseWithPrompt :: Prompt -> [String] -> [String]
+toUpperCaseWithPrompt prompt = (prompt :) . map ((++ '\n':prompt) . upcase) 
+
+foo :: IO ()
+foo = genericInteract inputLines 
+                      outputLines 
+                      toUpperCase
+
+foo' :: IO ()
+foo' = genericInteract inputLines 
+                       outputLinesWithPrompt
+                       (toUpperCaseWithPrompt "? ")
+
+bar :: IO ()
+bar = genericInteract (inputLines' defaultInputConfig) 
+                      outputLines
+                      toUpperCase
+
+bar' :: IO ()
+bar' = genericInteract (inputLines' defaultInputConfig)
+                       outputLinesWithPrompt
+                       (toUpperCaseWithPrompt "? ")
+
+baz :: IO ()
+baz = do
+    { chan <- newChan
+    ; genericInteract inputLines
+                      (outputLines' chan)
+                      toUpperCase
+    }
+baz' :: IO ()
+baz' = do
+    { chan <- newChan
+    ; genericInteract inputLines
+                      (outputLinesWithPrompt' chan) 
+                      (toUpperCaseWithPrompt "? ") 
     }
 
-sample :: [String] -> [String]
-sample = ((toUpper <$>) <$>)
+qux :: IO ()
+qux = do
+    { chan <- newChan
+    ; genericInteract (inputLines' defaultInputConfig)
+                      (outputLines' chan)
+                      toUpperCase
+    }
+
+qux' :: IO ()
+qux' = do
+    { chan <- newChan
+    ; genericInteract (inputLines' defaultInputConfig)
+                      (outputLinesWithPrompt' chan)
+                      (toUpperCaseWithPrompt "? ")
+    }
